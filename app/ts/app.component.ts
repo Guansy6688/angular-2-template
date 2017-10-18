cd2c2d689b6fac81e590ccebd7340b073ca5d949
Guansy6688 <Guansy6688@users.noreply.github.com>
2017-10-17 21:50:28 -0400

    Add files via upload

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,29 @@
 import {Component} from 'angular2/core';
+import {Config}  from './config.service';
+import {Video} from './video';
+import {VideoPlayerComponent} from './videoplayer.component';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives:[PlaylistComponent, VideoPlayerComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeadings = Config.MAIN_HEADING;
+    videos:Array<Video>;
+    video: Video;
+
+    constructor(){
+        this.videos = [
+            new Video(1,"Tutorial 7", "f8qBeaGe2S4","Nested Components"),
+            new Video(2,"Tutorial 8", "bKWDKmHvF78","Passing Data to Components"),
+
+        ];
+        this.video = this.videos[0];
+    }
+    onVideoSelected(video: Video) {
+        this.video = video;
+    }
+}
